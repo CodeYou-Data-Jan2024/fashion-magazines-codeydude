01e24d895daab598cae068887b77f029d5f42f16
Lavin Lalchandani <lavinl27@gmail.com>
2024-06-21 16:52:55 -0400

    update

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,11 @@
--- Add your sql here
+SELECT *, PRINTF($%.2f, SUM(S.subscription_ength * S.price_per_month)) AS "Amount Due"
+FROM Customers AS C
+JOIN Orders AS O
+    ON C.customer_id = O.customer_id
+
+JOIN Subscriptions AS S
+    ON O.subscription_id = S.subscription_id
+
+WHERE S.description = "Fashion Magazine"
+AND O.order_status = "Unpaid"
+GROUP BY C.customer_name;
