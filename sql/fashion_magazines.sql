9080d65881425f169b652a23543ff489a64200a3
Lavin Lalchandani <lavinl27@gmail.com>
2024-06-21 17:19:59 -0400

    complete

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,11 +1,12 @@
-SELECT *, PRINTF('$%.2f', SUM(S.subscription_length * S.price_per_month)) AS "Amount Due"
-FROM orders AS O
-JOIN customers AS C
-    ON C.customer_id = O.customer_id
+SELECT customers.customer_name AS "Customer", 
+PRINTF('$%.2f', SUM(subscriptions.subscription_length * subscriptions.price_per_month)) AS "Amount Due"
+FROM orders
+JOIN customers
+    ON orders.customer_id = customers.customer_id
 
-JOIN subscriptions AS S
-    ON O.subscription_id = S.subscription_id
+JOIN subscriptions
+    ON orders.subscription_id = subscriptions.subscription_id
 
-WHERE S.description = "Fashion Magazine"
-AND O.order_status = "Unpaid"
-GROUP BY C.customer_name;
+WHERE subscriptions.description = "Fashion Magazine"
+    AND orders.order_status = "unpaid"
+GROUP BY customers.customer_name
